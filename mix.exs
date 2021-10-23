113b7fe2f4c5c6521f048147343e8bf271af463f
Max Veytsman <maxim@ontoillogical.com>
2021-10-23 04:09:32 +0200

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -38,16 +38,16 @@ defmodule BikeBrigade.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.0-rc.0", override: true},
+      {:phoenix, "~> 1.6.2"},
       {:phoenix_ecto, "~> 4.4.0"},
       {:ecto_sql, "~> 3.6.2"},
       {:ecto_psql_extras, "~> 0.2"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_view, "~> 0.16.4"},
+      {:phoenix_live_view, "~> 0.17.2"},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 3.0.4"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "0.5.1"},
+      {:phoenix_live_dashboard, "~> 0.6.0"},
       {:reverse_proxy_plug, "~> 2.0"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
