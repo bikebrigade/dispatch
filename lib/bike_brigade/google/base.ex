2f4c318eca4275a9723c6e532ab4848bff429c1d
Benjamin Moss <drteeth@gmail.com>
2021-11-02 16:27:14 -0400

    Allow Google credentials to be base64 encoded

--- lib/bike_brigade/google/base.ex
+++ lib/bike_brigade/google/base.ex
@@ -11,6 +11,8 @@ defmodule BikeBrigade.Google do
     children ++ do_append(credentials, config[:start])
   end
 
+  # N.B.: will start given any other value than `false`
+  # `nil` indicates we should startup the Goth server
   defp do_append(nil, _), do: []
   defp do_append(_credentials, false), do: []
 
@@ -23,8 +25,13 @@ defmodule BikeBrigade.Google do
     credentials =
       case credentials
            |> Jason.decode() do
-        {:ok, credentials} -> credentials
-        {:error, _err} -> nil
+        {:ok, credentials} ->
+          credentials
+
+        {:error, %Jason.DecodeError{data: base64}} ->
+          base64
+          |> Base.decode64!()
+          |> Jason.decode!()
       end
 
     {:service_account, credentials, scopes: @scopes}
