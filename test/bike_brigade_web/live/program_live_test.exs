f98687031cf2bcb5a852d10f61b4f9d60d0ab128
Diana Kagotho <dianakago2@gmail.com>
2021-11-17 13:39:12 -0500

    Added program editing test

--- test/bike_brigade_web/live/program_live_test.exs
+++ test/bike_brigade_web/live/program_live_test.exs
@@ -3,6 +3,8 @@ defmodule BikeBrigadeWeb.ProgramLiveTest do
 
   import Phoenix.LiveViewTest
 
+  alias BikeBrigade.Delivery
+
   describe "Index" do
     setup [:create_program, :login]
 
@@ -23,5 +25,38 @@ defmodule BikeBrigadeWeb.ProgramLiveTest do
 
       assert_redirected(view, "/programs/#{program.id}")
     end
+
+     # Click on Edit
+
+    test "redirects to edit program", %{conn: conn, program: program} do
+      {:ok, view, _html} = live(conn, Routes.program_index_path(conn, :index))
+
+      view
+      |> element("##{program.id} a", "Edit")
+      |> render_click()
+
+      assert_patched(view, "/programs/#{program.id}/edit")
+    end
+
+    # Edit form
+
+    test "can edit a program", %{conn: conn, program: program} do
+      {:ok, view, html} = live(conn, Routes.program_show_path(conn, :edit, program))
+
+      {:ok, view, html} =
+      view
+        |> form("#program-form",
+          program_form: %{program: %{name: "Foodies", campaign_blurb: "Lorem Ipsum is simply dummy text", description: "Food for all foodies", contact_name: "Zizo", contact_email: "zizo@gmail.com"}}
+        )
+        |> render_submit()
+        |> follow_redirect(conn)
+
+        assert html =~ "Foodies"
+
+        updated_program = Delivery.get_program!(program.id) #get the program that had an id of 1
+        assert updated_program.name == "Foodies"
+
+    end
+
   end
 end
