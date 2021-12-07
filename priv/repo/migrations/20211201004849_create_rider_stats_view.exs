ba5e9ec2182f8ff2645f987c0b04e32462320862
Max Veytsman <maxim@ontoillogical.com>
2021-12-07 13:40:26 -0500

    Moved views to migrations
    Added check for campaign time being <= now
    
    Co-authored-by: Jenna Blumenthal <jennaleeblumenthal@gmail.com>

--- priv/repo/migrations/20211201004849_create_rider_stats_view.exs
+++ priv/repo/migrations/20211201004849_create_rider_stats_view.exs
@@ -1,21 +1,25 @@
 defmodule BikeBrigade.Repo.Migrations.CreateRiderStatsView do
   use Ecto.Migration
 
-  def up do
-    execute view("rider_stats.sql")
-  end
+  @view """
+  create or replace view rider_stats as
+  select
+    assigned_rider_id as rider_id,
+    count(tasks.id) as task_count,
+    sum(delivery_distance) as total_distance,
+    count(distinct campaigns.id) as campaign_count,
+    count(distinct program_id) as program_count
+  from
+    tasks
+  left join
+    campaigns on campaigns.id = tasks.campaign_id
+  where
+    campaigns.delivery_start <= NOW()
+  group by
+    assigned_rider_id;
+  """
 
-  def down do
-    execute "drop view rider_stats;"
-  end
-
-  defp view(view_filename) do
-    Path.join([
-      :code.priv_dir(:bike_brigade),
-      "repo",
-      "views",
-      view_filename
-    ])
-    |> File.read!()
+  def change do
+    execute @view, "drop view rider_stats;"
   end
 end
--- priv/repo/migrations/20211201004855_create_rider_program_stats_view.exs
+++ priv/repo/migrations/20211201004855_create_rider_program_stats_view.exs
@@ -1,21 +1,25 @@
 defmodule BikeBrigade.Repo.Migrations.CreateRiderProgramStatsView do
   use Ecto.Migration
 
+  @view """
+  create or replace view rider_program_stats as
+  select
+    assigned_rider_id as rider_id,
+    program_id,
+    count(tasks.id) as task_count,
+    sum(delivery_distance) as total_distance,
+    count(distinct campaigns.id) as campaign_count
+  from
+    tasks
+  left join
+    campaigns on campaigns.id = tasks.campaign_id
+  where
+    campaigns.delivery_start <= NOW()
+  group by
+    assigned_rider_id, program_id;
+  """
+
   def up do
-    execute view("rider_program_stats.sql")
-  end
-
-  def down do
-    execute "drop view rider_program_stats;"
-  end
-
-  defp view(view_filename) do
-    Path.join([
-      :code.priv_dir(:bike_brigade),
-      "repo",
-      "views",
-      view_filename
-    ])
-    |> File.read!()
+    execute @view, "drop view rider_program_stats;"
   end
 end
