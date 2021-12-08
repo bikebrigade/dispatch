18e67dd08209cd4479a43e3c44e8a7622a823bbc
Max Veytsman <maxim@ontoillogical.com>
2021-12-07 21:47:57 -0500

    Create table component

--- lib/bike_brigade_web/live/components/ui.ex
+++ lib/bike_brigade_web/live/components/ui.ex
@@ -186,4 +186,35 @@ defmodule BikeBrigadeWeb.Components.UI do
     </div>
     """
   end
+
+  def table(assigns) do
+    ~H"""
+    <div class="flex flex-col py-4">
+      <div class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+        <div class="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
+          <table class="min-w-full">
+            <thead>
+              <tr>
+                <%= for th <- @th do %>
+                <th class={"px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50 #{Map.get(th, :class)}"}>
+                  <%= render_slot(th) %>
+                </th>
+                <% end %>
+              </tr>
+            </thead>
+            <%= for row <- @rows do %>
+              <tr>
+                <%= for td <- @td do %>
+                  <td class={"px-6 py-4 text-sm leading-5 text-gray-500 border-b border-gray-200 whitespace-nowrap #{Map.get(td, :class)}"}>
+                    <%= render_slot(td, row) %>
+                  </td>
+                <% end %>
+              </tr>
+            <% end %>
+          </table>
+        </div>
+      </div>
+    </div>
+    """
+  end
 end
