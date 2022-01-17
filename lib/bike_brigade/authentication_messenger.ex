e3387d825c665affe9e8df3eed923ae93ed5691e
Max Veytsman <maxim@ontoillogical.com>
2022-01-17 12:41:14 -0500

    Set up authentication messenger

--- lib/bike_brigade/authentication_messenger.ex
+++ lib/bike_brigade/authentication_messenger.ex
@@ -8,20 +8,40 @@ defmodule BikeBrigade.AuthenticationMessenger do
 
   require Logger
 
+  @name {:via, Horde.Registry, {BikeBrigade.HordeRegistry, __MODULE__}}
+
   # Client
 
-  def start_link(opts \\ []) do
-    opts = Keyword.put_new(opts, :name, __MODULE__)
-    GenServer.start_link(opts[:name], %{}, opts)
+  def start_link([]) do
+    # When we are in non-distributed mode start us using Horde
+    # TODO: this + @name can be abstracted to a Singleton Genserver or the like
+    Horde.DynamicSupervisor.start_child(
+      BikeBrigade.HordeSupervisor,
+      {__MODULE__, distributed: true}
+    )
+
+    # Ignore since this is called from the main supervisor
+    :ignore
   end
 
-  def generate_token(phone), do: generate_token(__MODULE__, phone)
+  def start_link(distributed: true) do
+    case GenServer.start_link(__MODULE__, %{}, name: @name) do
+      {:ok, pid} ->
+        {:ok, pid}
+
+      {:error, {:already_started, pid}} ->
+        Logger.info("#{inspect(__MODULE__)}: already started at #{inspect(pid)}, returning :ignore")
+        :ignore
+    end
+  end
+
+  def generate_token(phone), do: generate_token(@name, phone)
 
   def generate_token(pid, phone) do
     GenServer.call(pid, {:generate_token, phone})
   end
 
-  def validate_token(phone, token_attempt), do: validate_token(__MODULE__, phone, token_attempt)
+  def validate_token(phone, token_attempt), do: validate_token(@name, phone, token_attempt)
 
   def validate_token(pid, phone, token_attempt) when is_binary(token_attempt) do
     try do
@@ -53,8 +73,8 @@ defmodule BikeBrigade.AuthenticationMessenger do
         Process.send_after(self(), {:expire, phone}, 60000)
         {:reply, :ok, Map.put(state, phone, token)}
 
-      {:error, err, _code} ->
-        {:reply, {:error, "Twilio error: #{Map.get(err, "message", "unknown")}"}, state}
+      {:error, error} ->
+        {:reply, {:error, "Twilio error: #{error}"}, state}
     end
   end
 
@@ -81,7 +101,6 @@ defmodule BikeBrigade.AuthenticationMessenger do
     {:noreply, Map.delete(state, phone)}
   end
 
-  # TODO move this to a module that wraps extwilio
   defp send_message(phone, token) do
     msg = [
       from: Messaging.outbound_number(),
