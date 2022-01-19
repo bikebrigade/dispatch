c160a57643f96da5f2ff9e7ab11bf90e1bc8453d
Max Veytsman <maxim@ontoillogical.com>
2022-01-18 19:19:11 -0500

    Bulk messaging

--- lib/bike_brigade_web/live/rider_live/index_next.ex
+++ lib/bike_brigade_web/live/rider_live/index_next.ex
@@ -6,12 +6,75 @@ defmodule BikeBrigadeWeb.RiderLive.IndexNext do
   alias BikeBrigade.Delivery
   alias BikeBrigade.LocalizedDateTime
 
-  @impl true
+  @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
     {:ok,
      socket
      |> assign(:riders, fetch_riders())
-     |> assign(:page, :riders)}
+     |> assign(:page, :riders)
+     |> assign(:selected, MapSet.new())}
+  end
+
+  @impl Phoenix.LiveView
+
+  def handle_event(
+        "select",
+        %{"_target" => ["selected", "all"], "selected" => %{"all" => select_all}},
+        socket
+      ) do
+    selected =
+      case select_all do
+        "true" ->
+          for r <- socket.assigns.riders, into: MapSet.new(), do: r.id
+
+        "false" ->
+          MapSet.new()
+      end
+
+    {:noreply, assign(socket, :selected, selected)}
+  end
+
+  def handle_event(
+        "select",
+        %{"_target" => ["selected", id], "selected" => selected_params},
+        socket
+      ) do
+    selected =
+      case selected_params[id] do
+        "true" ->
+          MapSet.put(socket.assigns.selected, String.to_integer(id))
+
+        "false" ->
+          MapSet.delete(socket.assigns.selected, String.to_integer(id))
+      end
+
+    {:noreply, assign(socket, :selected, selected)}
+  end
+
+  def handle_event(
+        "select",
+        %{"_target" => ["selected", "all"], "selected" => selected_params},
+        socket
+      ) do
+    %{opportunities: opportunities} = socket.assigns
+
+    selected =
+      case selected_params["all"] do
+        "true" -> Enum.map(opportunities, & &1.id) |> MapSet.new()
+        "false" -> MapSet.new()
+      end
+
+    {:noreply, assign(socket, :selected, selected)}
+  end
+
+  def handle_event("bulk-message", _params, socket) do
+    rider_ids = socket.assigns.selected |> MapSet.to_list()
+
+    {:noreply,
+     push_redirect(socket,
+       to: Routes.sms_message_index_path(socket, :new, r: rider_ids),
+       replace: false
+     )}
   end
 
   defp fetch_riders() do
@@ -28,11 +91,26 @@ defmodule BikeBrigadeWeb.RiderLive.IndexNext do
     """
   end
 
+  @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
     <div>
-      <UI.table rows={@riders}>
-        <:th>
+      <div class="flex justify-between">
+        <input type="text" phx-keydown="search-riders" phx-debounce="100"
+          value={}
+          class="block w-2/3 px-3 py-2 placeholder-gray-400 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          placeholder="Name, email, phone, tag, neighborhood" />
+        <C.button phx-click="bulk-message">Bulk Message</C.button>
+      </div>
+      <form id="selected" phx-change="select"></form>
+      <UI.table rows={@riders} class="mt-2">
+        <:th class="text-center" padding="px-3">
+        <%= checkbox :selected, :all,
+          form: "selected",
+          value: Enum.count(@selected) == Enum.count(@riders),
+          class: "w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500" %>
+        </:th>
+        <:th padding="px-3">
           Name
         </:th>
         <:th>
@@ -45,7 +123,13 @@ defmodule BikeBrigadeWeb.RiderLive.IndexNext do
           Last Active
         </:th>
 
-        <:td let={rider}>
+        <:td let={rider} class="text-center" padding="px-3">
+          <%= checkbox :selected, "#{rider.id}",
+            form: "selected",
+            value: MapSet.member?(@selected, rider.id),
+            class: "w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500" %>
+        </:td>
+        <:td let={rider} padding="px-3">
           <%= live_redirect to: Routes.rider_show_path(@socket, :show, rider), class: "link" do %>
             <%= rider.name %><span class="ml-1 text-xs lowercase ">(<%= rider.pronouns %>)</span>
           <% end %>
