cc6160a2ca4ff17a244cba7dac3dab778331e9fa
Max Veytsman <maxim@ontoillogical.com>
2022-01-21 03:35:51 -0500

    Improve bulk message UI

--- lib/bike_brigade_web/live/components/rider_selection_component.ex
+++ lib/bike_brigade_web/live/components/rider_selection_component.ex
@@ -43,91 +43,66 @@ defmodule BikeBrigadeWeb.Components.RiderSelectionComponent do
   def render(assigns) do
     ~H"""
     <div id="rider-select">
-    <%= for rider <- @selected_riders do %>
-      <div class="flex px-3 py-4">
-        <div class="flex items-center flex-1 min-w-0">
-          <div class="flex-shrink-0">
-            <img class="w-12 h-12 rounded-full" src={ gravatar(rider.email) } alt="" />
-          </div>
-          <div class="ml-2">
-            <div class="text-sm font-medium leading-5 text-indigo-600">
-              <span><%= rider.name %></span>
-              <span class="ml-1 font-normal text-gray-500">
-                <%= rider.pronouns %>
-              </span>
-            </div>
-            <div class="flex items-center mt-2 text-sm leading-5 text-gray-500">
-              <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
-              </svg>
-              <span class="truncate">
-                <%= rider.phone %>
-              </span>
-            </div>
-            <div class="flex items-center mt-2 text-sm text-gray-500">
-              <!-- Heroicon name: solid/mail -->
-              <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-              </svg>
-              <span class="truncate"><%= rider.email %></span>
-            </div>
-          </div>
-        </div>
-        <div>
-          <a href="#" phx-click="unselect" phx-value-id={ rider.id } phx-target={ @myself }  class="block text-sm text-gray-400 bg-white rounded-md font-base hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            <!-- Heroicon name: outline/x -->
-              <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </a>
-        </div>
+      <div class="grid grid-cols-2">
+        <%= for rider <- @selected_riders do %>
+          <.show rider={rider}>
+            <:x>
+              <a href="#" phx-click="unselect" phx-value-id={ rider.id } phx-target={ @myself }  class="block text-sm text-gray-400 bg-white rounded-md font-base hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <Heroicons.Outline.x class="w-5 h-5" />
+              </a>
+            </:x>
+          </.show>
+          <input type="hidden" name={ @input_name } value={ rider.id }>
+        <% end %>
       </div>
-      <input type="hidden" name={ @input_name } value={ rider.id }>
-    <% end %>
     <%= if @multi || Enum.empty?(@selected_riders) do %>
       <input type="text" phx-keyup="suggest" phx-target={ @myself } phx-debounce="50" name="search" placeholder="Type to search for riders by name"
       class="block w-full px-3 py-2 placeholder-gray-400 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
       <ul id="rider-selection-list" class="overflow-y-auto max-h-64">
         <%= for rider <- @riders do %>
           <li id={"rider-selection:#{rider.id}"}>
-          <a href="#" phx-click="select" phx-value-id={ rider.id } phx-target={ @myself } class="block transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus:bg-gray-50">
-            <div class="flex items-center px-3 py-4">
-              <div class="flex items-center flex-1 min-w-0">
-                <div class="flex-shrink-0">
-                  <img class="w-12 h-12 rounded-full" src={ gravatar(rider.email) } alt="" />
-                </div>
-                <div class="ml-2">
-                  <div class="text-sm font-medium leading-5 text-indigo-600">
-                    <span><%= rider.name %></span>
-                    <span class="ml-1 font-normal text-gray-500">
-                      <%= rider.pronouns %>
-                    </span>
-                  </div>
-                  <div class="flex items-center mt-2 text-sm leading-5 text-gray-500">
-                    <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                      <path d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
-                    </svg>
-                    <span class="truncate">
-                      <%= rider.phone %>
-                    </span>
-                  </div>
-                  <div class="flex items-center mt-2 text-sm text-gray-500">
-                    <!-- Heroicon name: solid/mail -->
-                    <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                      <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                      <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                    </svg>
-                    <span class="truncate"><%= rider.email %></span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </a>
+            <a href="#" phx-click="select" phx-value-id={ rider.id } phx-target={ @myself } class="block transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus:bg-gray-50">
+              <.show rider={rider}/>
+            </a>
           </li>
         <% end %>
       </ul>
     <% end %>
+    </div>
+    """
+  end
+
+  defp show(assigns) do
+    assigns =
+      assigns
+      |> assign_new(:x, fn -> [] end)
+
+    ~H"""
+    <div class="flex items-start px-3 py-4">
+      <div class="flex items-center flex-1 min-w-0">
+        <div class="flex-shrink-0">
+          <img class="w-12 h-12 rounded-full" src={ gravatar(@rider.email) } alt="" />
+        </div>
+        <div class="ml-2">
+          <div class="text-sm font-medium leading-5 text-indigo-600">
+            <span><%= @rider.name %></span>
+            <span class="ml-1 text-xs font-normal text-gray-500">
+              <%= @rider.pronouns %>
+            </span>
+          </div>
+          <div class="flex items-center mt-2 text-xs leading-5 text-gray-500">
+            <Heroicons.Solid.phone class="flex-shrink-0 w-4 h-4 mr-1 text-gray-400" />
+            <span class="truncate">
+              <%= @rider.phone %>
+            </span>
+          </div>
+          <div class="flex items-center mt-2 text-xs text-gray-500">
+            <Heroicons.Solid.mail class="flex-shrink-0 w-4 h-4 mr-1 text-gray-400" />
+            <span class="truncate"><%= @rider.email %></span>
+          </div>
+        </div>
+      </div>
+      <%= render_slot(@x) %>
     </div>
     """
   end
