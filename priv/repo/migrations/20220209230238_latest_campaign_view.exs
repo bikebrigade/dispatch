f832ea2b50dd2d3be78cbdf4e102d4c9d4169eea
Max Veytsman <maxim@ontoillogical.com>
2022-02-10 18:11:57 -0500

    oops! fix broken migration

--- priv/repo/migrations/20220209230238_latest_campaign_view.exs
+++ priv/repo/migrations/20220209230238_latest_campaign_view.exs
@@ -1,6 +1,6 @@
 defmodule BikeBrigade.Repo.Migrations.LatestCampaignView do
   use Ecto.Migration
-  import BikeBrigade.Repo.Helpers
+  import BikeBrigade.MigrationUtils
 
   def change do
     load_sql("programs_latest_campaigns_view.sql", "drop view if exists programs_latest_campaigns")
