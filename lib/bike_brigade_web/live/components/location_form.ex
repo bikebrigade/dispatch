44e9d7ba1e66d4f4af87c4cff5adbf64ab699779
Max Veytsman <maxim@ontoillogical.com>
2022-02-22 17:33:50 -0500

    Fix bug in location form

--- lib/bike_brigade_web/live/components/location_form.ex
+++ lib/bike_brigade_web/live/components/location_form.ex
@@ -4,7 +4,7 @@ defmodule BikeBrigadeWeb.Components.LocationForm do
   def component(assigns) do
     ~H"""
     <div class="my-2">
-      <%= hidden_input @for, :coords, value: Jason.encode!(coords(@for))  %>
+      <%= hidden_input @for, :coords, value: dump_coords(@for)  %>
       <%= hidden_input @for, :neighborhood %>
       <%= hidden_input @for, :postal %>
       <%= hidden_input @for, :city  %>
@@ -41,13 +41,20 @@ defmodule BikeBrigadeWeb.Components.LocationForm do
             </div>
           </div>
         </div>
-        <C.map coords={coords(@for)} class="w-full h-64 mt-2"/>
+        <C.map coords={cast_coords(@for)} class="w-full h-64 mt-2"/>
       </div>
     </div>
     """
   end
 
-  defp coords(form) do
+  defp dump_coords(form) do
+    case input_value(form, :coords) do
+      %Geo.Point{} = coords -> coords |> Geo.JSON.encode!() |> Jason.encode!()
+      json when is_binary(json) -> json
+    end
+  end
+
+  defp cast_coords(form) do
     case input_value(form, :coords) do
       %Geo.Point{} = coords -> coords
       json when is_binary(json) -> Jason.decode!(json) |> Geo.JSON.decode!()
