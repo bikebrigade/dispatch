163b809f1ac37d0cfe64347fffdd188068fe233c
Max Veytsman <maxim@ontoillogical.com>
2022-03-14 17:52:55 -0400

    Fix issue with view

--- priv/repo/sql/campaign_stats_view.sql
+++ priv/repo/sql/campaign_stats_view.sql
@@ -1,17 +1,15 @@
 create
 or replace view campaign_stats as
 select
-  campaigns.program_id as program_id, -- useful if we ever need program stats
+  programs.id as program_id,
   campaigns.id as campaign_id,
   count(distinct campaigns.id) as campaign_count, -- useful if we ever need program stats
   count(tasks.id) as task_count,
   count(distinct tasks.assigned_rider_id) as rider_count,
   sum(delivery_distance) as total_distance
 from
-  tasks
-  left join campaigns on campaigns.id = tasks.campaign_id
-where
-  tasks.assigned_rider_id IS NOT NULL
-  and campaigns.delivery_start <= NOW()
+  programs
+  left join campaigns on campaigns.program_id = programs.id
+  left join tasks on tasks.campaign_id = campaigns.id
 group by
-  ROLLUP(program_id, campaigns.id)
+  ROLLUP(programs.id, campaigns.id)
