3b28b1a49df5075fa718a0443d5e411840a50880
Max Veytsman <maxim@ontoillogical.com>
2022-03-15 17:11:01 -0400

    Get signed up rider count

--- lib/bike_brigade/stats/campaign_stats.ex
+++ lib/bike_brigade/stats/campaign_stats.ex
@@ -7,7 +7,8 @@ defmodule BikeBrigade.Stats.CampaignStats do
   @primary_key false
   schema "campaign_stats" do
     field :task_count, :integer, default: 0
-    field :rider_count, :integer, default: 0
+    field :assigned_rider_count, :integer, default: 0
+    field :signed_up_rider_count, :integer, default: 0
     field :total_distance, :integer, default: 0
     field :campaign_count, :integer, default: 0
 
--- priv/repo/sql/campaign_stats_view.sql
+++ priv/repo/sql/campaign_stats_view.sql
@@ -5,12 +5,14 @@ select
   campaigns.id as campaign_id,
   count(distinct campaigns.id) as campaign_count,
   count(tasks.id) as task_count,
-  count(distinct tasks.assigned_rider_id) as rider_count,
+  count(distinct tasks.assigned_rider_id) as assigned_rider_count,
+  count(distinct campaigns_riders.rider_id) as signed_up_rider_count,
   sum(st_distance(pickup_locations.coords, dropoff_locations.coords))::integer as total_distance
 from
   programs
   left join campaigns on campaigns.program_id = programs.id
   left join tasks on tasks.campaign_id = campaigns.id
+  left join campaigns_riders on campaigns_riders.campaign_id = campaigns.id
   left join locations pickup_locations on pickup_locations.id = tasks.pickup_location_id
   left join locations dropoff_locations on dropoff_locations.id = tasks.dropoff_location_id
 group by
