4e3aa997d8e62684964610c2fc80dd70fca3e47e
Max Veytsman <maxim@ontoillogical.com>
2022-05-06 11:32:50 -0400

    Make seeds idempotent

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,6 +1,5 @@
-alias BikeBrigade.Repo
-alias BikeBrigade.Delivery.Campaign
-alias BikeBrigade.Riders.Rider
+alias BikeBrigade.Accounts
+alias BikeBrigade.Riders
 alias BikeBrigade.Utils
 alias BikeBrigade.Repo.Seeds.Toronto
 
@@ -8,31 +7,33 @@ alias BikeBrigade.LocalizedDateTime
 
 # Create a dispatcher
 
-{:ok, _} =
+if Accounts.list_users() == [] do
   BikeBrigade.Accounts.create_user(%{
     name: "Dispatcher McGee",
     phone: "647-555-5555",
     email: "dispatcher@example.com"
   })
+end
 
 # Create some riders
+if Riders.list_riders() == [] do
+  for _ <- 0..100 do
+    location = Toronto.random_location()
 
-for _ <- 0..100 do
-  location = Toronto.random_location()
-
-  {:ok, _rider} =
-    BikeBrigade.Riders.create_rider(%{
-      address: location.address,
-      # TODO
-      availability: %{},
-      capacity: Utils.random_enum(Rider.CapacityEnum),
-      email: Faker.Internet.email(),
-      location: location,
-      max_distance: 20,
-      name: "#{Faker.Person.first_name()} #{Faker.Person.last_name()}",
-      phone: "647-#{Enum.random(200..999)}-#{Enum.random(1000..9999)}",
-      postal: location.postal,
-      pronouns: Enum.random(~w(He/Him She/Her They/Them)),
-      signed_up_on: LocalizedDateTime.now()
-    })
+    {:ok, _rider} =
+      BikeBrigade.Riders.create_rider(%{
+        address: location.address,
+        # TODO
+        availability: %{},
+        capacity: Utils.random_enum(Riders.Rider.CapacityEnum),
+        email: Faker.Internet.email(),
+        location: location,
+        max_distance: 20,
+        name: "#{Faker.Person.first_name()} #{Faker.Person.last_name()}",
+        phone: "647-#{Enum.random(200..999)}-#{Enum.random(1000..9999)}",
+        postal: location.postal,
+        pronouns: Enum.random(~w(He/Him She/Her They/Them)),
+        signed_up_on: LocalizedDateTime.now()
+      })
+  end
 end
