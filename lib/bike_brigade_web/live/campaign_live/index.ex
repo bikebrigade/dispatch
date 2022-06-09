d13b7b598639fc599fc9ec89872ec00d62d684e7
Marius Butuc <marius.butuc@gmail.com>
2022-06-09 15:39:16 -0400

    Extract default delivery start & end for new campaign

--- lib/bike_brigade_web/live/campaign_live/index.ex
+++ lib/bike_brigade_web/live/campaign_live/index.ex
@@ -70,16 +70,11 @@ defmodule BikeBrigadeWeb.CampaignLive.Index do
   end
 
   defp apply_action(socket, :new, _params) do
-    today = LocalizedDateTime.today()
-    start_time = ~T[17:00:00]
-    end_time = ~T[19:30:00]
-
-    delivery_start = LocalizedDateTime.new!(today, start_time)
-    delivery_end = LocalizedDateTime.new!(today, end_time)
+    campaign = Delivery.new_campaign()
 
     socket
     |> assign(:page_title, "New Campaign")
-    |> assign(:campaign, %Campaign{delivery_start: delivery_start, delivery_end: delivery_end})
+    |> assign(:campaign, campaign)
   end
 
   defp apply_action(socket, :duplicate, %{"id" => id}) do
--- lib/bike_brigade_web/live/program_live/show.ex
+++ lib/bike_brigade_web/live/program_live/show.ex
@@ -2,8 +2,6 @@ defmodule BikeBrigadeWeb.ProgramLive.Show do
   use BikeBrigadeWeb, :live_view
 
   alias BikeBrigade.Delivery
-  alias BikeBrigade.Delivery.Campaign
-  alias BikeBrigade.LocalizedDateTime
   import BikeBrigadeWeb.CampaignHelpers
 
   @impl true
@@ -31,20 +29,10 @@ defmodule BikeBrigadeWeb.ProgramLive.Show do
 
   def apply_action(socket, :new_campaign, %{"id" => id}) do
     program = Delivery.get_program!(id, preload: [:lead, :items, campaigns: [:stats]])
-
-    today = LocalizedDateTime.today()
-    start_time = ~T[17:00:00]
-    end_time = ~T[19:30:00]
-
-    delivery_start = LocalizedDateTime.new!(today, start_time)
-    delivery_end = LocalizedDateTime.new!(today, end_time)
+    campaign = Delivery.new_campaign(%{program_id: program.id})
 
     socket
     |> assign(:program, program)
-    |> assign(:campaign, %Campaign{
-      delivery_start: delivery_start,
-      delivery_end: delivery_end,
-      program_id: program.id
-    })
+    |> assign(:campaign, campaign)
   end
 end
