6d66af5bf2c384c07d692214e0a85741b0dcf526
nathan-louie <nathanhlouie@gmail.com>
2022-08-05 11:09:24 -0400

    Add tests

--- test/bike_brigade_web/live/itinerary_live_test.exs
+++ test/bike_brigade_web/live/itinerary_live_test.exs
@@ -2,6 +2,7 @@ defmodule BikeBrigadeWeb.RiderLiveTest do
   use BikeBrigadeWeb.ConnCase
 
   alias BikeBrigade.Accounts
+  alias BikeBrigade.LocalizedDateTime
 
   import Phoenix.LiveViewTest
 
@@ -18,7 +19,7 @@ defmodule BikeBrigadeWeb.RiderLiveTest do
       assert html =~ "User is not associated with a rider!"
     end
 
-    test "Doesn't have erorr when user has a rider", %{conn: conn, user: user, rider: rider} do
+    test "Doesn't have error when user has a rider", %{conn: conn, user: user, rider: rider} do
       # Associate the user with a rider
       # In the future the fixture may do this for us
       {:ok, user} = Accounts.update_user_as_admin(user, %{rider_id: rider.id})
@@ -30,6 +31,43 @@ defmodule BikeBrigadeWeb.RiderLiveTest do
 
       assert html =~ "Itinerary"
       refute html =~ "User is not associated with a rider!"
+    end
+
+    test "No campaigns for today's date", %{conn: conn, user: user, rider: rider} do
+      # Associate the user with a rider
+      # In the future the fixture may do this for us
+      {:ok, user} = Accounts.update_user_as_admin(user, %{rider_id: rider.id})
+
+      # Make sure we actually have a rider
+      assert user.rider_id != nil
+
+      {:ok, _index_live, html} = live(conn, Routes.itinerary_index_path(conn, :index))
+
+      assert html =~ "Itinerary"
+      assert html =~ Calendar.strftime(LocalizedDateTime.today(), "%A %B %-d, %Y")
+      assert html =~ "No campaigns found for this day."
+    end
+
+    test "Go to previous day", %{conn: conn, user: user, rider: rider} do
+      # Associate the user with a rider
+      # In the future the fixture may do this for us
+      {:ok, user} = Accounts.update_user_as_admin(user, %{rider_id: rider.id})
+
+      # Make sure we actually have a rider
+      assert user.rider_id != nil
+
+      {:ok, view, html} = live(conn, Routes.itinerary_index_path(conn, :index))
+
+      assert html =~ "Itinerary"
+
+      html =
+        view
+        |> element("[aria-label='Previous Day']")
+        |> render_click()
+
+      previous_day = Date.add(LocalizedDateTime.today(), -1)
+
+      assert html =~ Calendar.strftime(previous_day, "%A %B %-d, %Y")
     end
   end
 end
