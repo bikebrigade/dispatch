2ba69ac93ea2fcfb5faec6d44991cdd084f0d2cc
Max Veytsman <maxim@ontoillogical.com>
2022-08-05 15:04:25 -0400

    Rider login and profile pages! (#181)
    * Delete some old data migrations
    
    * Gate analytics to dispatchers
    
    * Create rider profile
    
    * Profile in sidebar

--- lib/bike_brigade_web/live/rider_live/show.ex
+++ lib/bike_brigade_web/live/rider_live/show.ex
@@ -19,36 +19,8 @@ defmodule BikeBrigadeWeb.RiderLive.Show do
   end
 
   @impl Phoenix.LiveView
-  def handle_params(%{"id" => id} = params, _url, socket) do
-    rider =
-      Riders.get_rider!(id)
-      |> Repo.preload([
-        :location,
-        :tags,
-        :campaigns,
-        :total_stats,
-        :user,
-        program_stats: [:program],
-        latest_campaign: [:program]
-      ])
-
-    today = LocalizedDateTime.today()
-    yesterday = Date.add(today, -1)
-    tomorrow = Date.add(today, 1)
-
-    schedule = [
-      {yesterday, Riders.list_campaigns_with_task_counts(rider, yesterday)},
-      {today, Riders.list_campaigns_with_task_counts(rider, today)},
-      {tomorrow, Riders.list_campaigns_with_task_counts(rider, tomorrow)}
-    ]
-
-    {:noreply,
-     socket
-     |> assign(:rider, rider)
-     |> assign(:stats, rider.total_stats || %RiderStats{})
-     |> assign(:today, today)
-     |> assign(:schedule, schedule)
-     |> apply_action(socket.assigns.live_action, params)}
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
   @impl Phoenix.LiveView
@@ -90,12 +62,69 @@ defmodule BikeBrigadeWeb.RiderLive.Show do
      |> assign(:schedule, schedule)}
   end
 
-  defp apply_action(socket, :edit, _) do
+  defp apply_action(socket, :profile, _params) do
     socket
-    |> assign(:return_to, Routes.rider_show_path(socket, :show, socket.assigns.rider))
+    |> assign(:page_title, "Profile")
+    |> assign(:page, :profile)
+    |> assign_rider(socket.assigns.current_user.rider_id)
+  end
+
+  defp apply_action(socket, :edit_profile, _params) do
+    socket
+    |> assign(:page_title, "Edit Profile")
+    |> assign(:page, :profile)
+    |> assign(
+      :return_to,
+      Routes.rider_show_path(socket, :profile)
+    )
+    |> assign_rider(socket.assigns.current_user.rider_id)
+  end
+
+  defp apply_action(socket, :show, %{"id" => id}) do
+    socket
+    |> assign_rider(id)
+  end
+
+  defp apply_action(socket, :edit, %{"id" => id}) do
+    socket
+    |> assign(
+      :return_to,
+      Routes.rider_show_path(socket, :show, socket.assigns.rider)
+    )
+    |> assign_rider(id)
   end
 
   defp apply_action(socket, _, _), do: socket
+
+  defp assign_rider(socket, id) do
+    rider =
+      Riders.get_rider!(id)
+      |> Repo.preload([
+        :location,
+        :tags,
+        :campaigns,
+        :total_stats,
+        :user,
+        program_stats: [:program],
+        latest_campaign: [:program]
+      ])
+
+    today = LocalizedDateTime.today()
+    yesterday = Date.add(today, -1)
+    tomorrow = Date.add(today, 1)
+
+    schedule = [
+      {yesterday, Riders.list_campaigns_with_task_counts(rider, yesterday)},
+      {today, Riders.list_campaigns_with_task_counts(rider, today)},
+      {tomorrow, Riders.list_campaigns_with_task_counts(rider, tomorrow)}
+    ]
+
+    socket
+    |> assign(:rider, rider)
+    |> assign(:stats, rider.total_stats || %RiderStats{})
+    |> assign(:today, today)
+    |> assign(:schedule, schedule)
+  end
 
   defp latest_campaign_info(assigns) do
     if assigns.rider.latest_campaign do
--- lib/bike_brigade_web/live_hooks/authentication.ex
+++ lib/bike_brigade_web/live_hooks/authentication.ex
@@ -4,21 +4,43 @@ defmodule BikeBrigadeWeb.LiveHooks.Authentication do
   alias BikeBrigade.Accounts
 
   def on_mount(:default, _params, %{"user_id" => user_id}, socket) do
-    # can this ever return nil?
-    user = Accounts.get_user(user_id)
+    if user = Accounts.get_user(user_id) do
+      # Set the context for Honeybadger here
+      Honeybadger.context(context: %{user_id: user.id, user_email: user.email})
 
-    # Set the context for Honeybadger here
-    Honeybadger.context(context: %{user_id: user.id, user_email: user.email})
-
-    {:cont,
-     socket
-     |> assign_new(:current_user, fn -> user end)
-     |> assign_new(:page_title, fn -> nil end)}
+      {:cont,
+       socket
+       |> assign_new(:current_user, fn -> user end)
+       |> assign_new(:page_title, fn -> nil end)}
+    else
+      {:halt,
+       socket
+       |> push_redirect(to: "/login")}
+    end
   end
 
   def on_mount(:default, _params, %{}, socket) do
     {:halt,
      socket
      |> push_redirect(to: "/login")}
+  end
+
+  def on_mount(:require_dispatcher, _params, %{"user_id" => user_id}, socket) do
+    case Accounts.get_user(user_id) do
+      %{is_dispatcher: true} = user ->
+        # Set the context for Honeybadger here
+        Honeybadger.context(context: %{user_id: user.id, user_email: user.email})
+
+        {:cont,
+         socket
+         |> assign_new(:current_user, fn -> user end)
+         |> assign_new(:page_title, fn -> nil end)}
+
+      _ ->
+        {:halt,
+         socket
+         |> put_flash(:error, "Must be a dispatcher to access this page")
+         |> push_redirect(to: "/login")}
+    end
   end
 end
