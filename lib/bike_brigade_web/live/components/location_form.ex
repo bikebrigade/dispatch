18aa9889d1fbe09733e9190e0910736e4aa26ccf
nathan-louie <nathanhlouie@gmail.com>
2022-08-12 15:33:08 -0400

    Add postal, city, province, country while editing location

--- lib/bike_brigade_web/live/components/location_form.ex
+++ lib/bike_brigade_web/live/components/location_form.ex
@@ -10,10 +10,6 @@ defmodule BikeBrigadeWeb.Components.LocationForm do
     ~H"""
     <div id={@id} class="my-2">
       <%= hidden_input(@for, :coords, value: dump_coords(@for)) %>
-      <%= hidden_input(@for, :postal) %>
-      <%= hidden_input(@for, :city) %>
-      <%= hidden_input(@for, :province) %>
-      <%= hidden_input(@for, :country) %>
       <div class="text-sm font-medium leading-5 text-gray-700">
         <%= @label %>
       </div>
@@ -68,6 +64,60 @@ defmodule BikeBrigadeWeb.Components.LocationForm do
                     "block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-blue focus:border-blue-300 sm:text-sm sm:leading-5"
                 ) %>
               </div>
+            </div>
+          </div>
+          <div class="flex mt-2 space-x-1">
+            <div class="w-1/4">
+              <label class="block text-xs font-medium leading-5 text-gray-700">
+                Postal Code
+              </label>
+              <div class="mt-1 rounded-md shadow-sm">
+                <%= text_input(@for, :postal,
+                  required: true,
+                  class:
+                    "block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+                ) %>
+              </div>
+              <%= error_tag(@for, :postal) %>
+            </div>
+            <div class="w-1/4">
+              <label class="block text-xs font-medium leading-5 text-gray-700">
+                City
+              </label>
+              <div class="mt-1 rounded-md shadow-sm">
+                <%= text_input(@for, :city,
+                  required: true,
+                  class:
+                    "block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+                ) %>
+              </div>
+              <%= error_tag(@for, :city) %>
+            </div>
+            <div class="w-1/4">
+              <label class="block text-xs font-medium leading-5 text-gray-700">
+                Province
+              </label>
+              <div class="mt-1 rounded-md shadow-sm">
+                <%= text_input(@for, :province,
+                  required: true,
+                  class:
+                    "block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+                ) %>
+              </div>
+              <%= error_tag(@for, :province) %>
+            </div>
+            <div class="w-1/4">
+              <label class="block text-xs font-medium leading-5 text-gray-700">
+                Country
+              </label>
+              <div class="mt-1 rounded-md shadow-sm">
+                <%= text_input(@for, :country,
+                  required: true,
+                  class:
+                    "block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:ring-blue focus:border-blue-300 sm:text-sm sm:leading-5"
+                ) %>
+              </div>
+              <%= error_tag(@for, :country) %>
             </div>
           </div>
           <C.map coords={cast_coords(@for)} class="w-full h-64 mt-2" />
