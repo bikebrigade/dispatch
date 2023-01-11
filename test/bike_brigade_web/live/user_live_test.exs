6e38c98f8d756f12031ef40741f25d3cdd7e16f8
teesloane <tyler.sloane@testdouble.com>
2023-01-11 15:36:33 -0500

    Test: rider can become user.

--- test/bike_brigade_web/live/user_live_test.exs
+++ test/bike_brigade_web/live/user_live_test.exs
@@ -22,24 +22,10 @@ defmodule BikeBrigadeWeb.UserLiveTest do
     end
   end
 
-  describe "Users :new / :edit" do
-    # REVIEW: users/new doesn't seem to be working locally yet.
-    @tag :skip
-    test "Can create a user", ctx do
-      {:ok, view, _html} = live(ctx.conn, ~p"/users/new")
-
-      res =
-        view
-        |> form("#user-form")
-        |> render_submit(%{
-          name: "Brandon",
-          email: "brandom@coolhats.com"
-        })
-    end
-
+  describe "Users new / edit" do
     test "Can edit a user", ctx do
-      {:ok, view, html} = live(ctx.conn, ~p"/users/#{ctx.user.id}/edit")
-      view |> element("#user-form") |> dbg()
+      {:ok, view, _html} = live(ctx.conn, ~p"/users/#{ctx.user.id}/edit")
+      view |> element("#user-form") 
 
       {:ok, _view, html} =
         view
@@ -53,6 +39,51 @@ defmodule BikeBrigadeWeb.UserLiveTest do
         |> follow_redirect(ctx.conn)
 
       assert html =~ ctx.user.name <> " the second"
+    end
+
+    test "Can make a rider into a user", ctx do
+      rider = fixture(:rider)
+      {:ok, view, html} = live(ctx.conn, ~p"/riders/#{rider.id}/show/edit")
+      assert html =~ "Login not Enabled"
+      view |> element("a", "Enable") |> render_click()
+      assert_redirected(view, ~p"/riders/#{rider.id}")
+
+      {:ok, _view, html} = live(ctx.conn, ~p"/users")
+
+      new_rider_user_row_text =
+        assert html
+               |> Floki.parse_fragment!()
+               |> Floki.find(".user-row")
+               |> List.last()
+               |> Floki.text()
+
+      assert new_rider_user_row_text =~ "🚴"
+      assert new_rider_user_row_text =~ rider.name
+    end
+
+    test "Can make a user into a dispatcher", ctx do
+      # first, upgrade a rider to a user.
+      rider = fixture(:rider)
+      {:ok, view, _html} = live(ctx.conn, ~p"/riders/#{rider.id}/show/edit")
+      view |> element("a", "Enable") |> render_click()
+
+      # find them in the users view,  and click "edit"
+      {:ok, view, _html} = live(ctx.conn, ~p"/users")
+      view |> element("a", ~r"Edit .* #{rider.name}") |> render_click()
+
+      {:ok, view, _html} =
+        view
+        |> form("#user-form", user: %{"is_dispatcher" => true})
+        |> render_submit()
+        |> follow_redirect(ctx.conn)
+
+      assert view
+             |> render()
+             |> Floki.parse_fragment!()
+             |> Floki.find(".user-row")
+             |> List.last()
+             |> Floki.text() =~
+               ~r/🧑‍🔧.*🚴/s
     end
   end
 end
