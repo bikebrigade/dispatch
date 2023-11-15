44899eb1f7139e70c2a3f0792603c82ea655d36f
teesloane <tyler.sloane@testdouble.com>
2023-11-15 09:01:19 -0500

    Remove no longer needed handle_event "add_rider"

--- lib/bike_brigade_web/live/campaign_live/add_rider_form_component.ex
+++ lib/bike_brigade_web/live/campaign_live/add_rider_form_component.ex
@@ -47,21 +47,6 @@ defmodule BikeBrigadeWeb.CampaignLive.AddRiderFormComponent do
   end
 
   @impl Phoenix.LiveComponent
-  def handle_event("add_rider", %{"campaign_rider" => cr_params}, socket) do
-    campaign = socket.assigns.campaign
-    attrs = Map.put(cr_params, "campaign_id", campaign.id)
-
-    case Delivery.create_campaign_rider(attrs) do
-      {:ok, _cr} ->
-        {:noreply,
-         socket
-         |> push_redirect(to: ~p"/campaigns/#{campaign}")}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :changeset, changeset)}
-    end
-  end
-
   def handle_event("save", %{"campaign_rider" => cr_params}, socket) do
     handle_save_impl(cr_params, socket, socket.assigns.action)
   end
