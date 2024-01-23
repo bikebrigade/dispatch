5dbca5a840966bfbcf36c5be58a3d8f7585884b4
tees <12987958+teesloane@users.noreply.github.com>
2024-01-23 15:57:33 -0500

    Refactor: rider links in sidebar (#266)
    * Add: dynamic_icon function.
    
    This allows us to render icons more declaritvely (ie, when looping
    through a data structure and spitting out html).
    
    * Refactor: rider links in sidebar
    
    - Now dispatchers can see rider links if they are a rider as well.
    
    * Fix: unify styles on navs between pages.
    
    * Remove toggling.

--- lib/bike_brigade_web/components/icons.ex
+++ lib/bike_brigade_web/components/icons.ex
@@ -89,4 +89,24 @@ defmodule BikeBrigadeWeb.Components.Icons do
     </svg>
     """
   end
+
+  attr :rest, :global,
+    doc: "the arbitrary HTML attributes for the svg container",
+    include: ~w(fill stroke stroke-width)
+
+  attr :name, :atom, required: true
+  attr :outline, :boolean, default: true
+  attr :solid, :boolean, default: false
+  attr :mini, :boolean, default: false
+
+  @doc """
+  A dynamic way of generating a Heroicon (v2).
+
+  Example:
+
+      <.icon name={:arrow_right} class="w-5 h-5 text-gray-600 dark:text-gray-400" />
+  """
+  def dynamic_icon(assigns) do
+    apply(Heroicons, assigns.name, [assigns])
+  end
 end
