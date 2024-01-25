e385f70d73949353329870977ef377820edf8381
teesloane <tyler.sloane@testdouble.com>
2024-01-24 19:58:37 -0500

    Add: CampaignMessageList def + impl.
    - This commit actual wires up the rendering of the UI and the logic for
    the new slide over messaging component.

---
+++ lib/bike_brigade_web/live/campaign_live/campaign_message_list.ex
@@ -0,0 +1,50 @@
+defmodule BikeBrigadeWeb.CampaignLive.CampaignMessageList do
+  @moduledoc """
+  This module handles the logic for showing a list of chat message
+  with the riders for a specific campaign.
+  """
+  use BikeBrigadeWeb, :live_component
+  alias BikeBrigadeWeb.CampaignHelpers
+
+  alias BikeBrigade.LocalizedDateTime
+
+  alias BikeBrigade.Messaging
+  alias BikeBrigade.Delivery
+
+  import BikeBrigade.Utils, only: [humanized_task_count: 1]
+  import BikeBrigadeWeb.DeliveryHelpers
+  import BikeBrigadeWeb.Components.SMSMessageListComponent
+
+  alias BikeBrigadeWeb.Components.ConversationComponent
+
+
+  def mount(socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def update(assigns, socket) do
+    rider_ids =
+      assigns.riders
+      |> Map.values()
+      |> Enum.map(&(&1.id))
+
+    conversations = Messaging.list_sms_for_campaign(rider_ids)
+
+    rider =
+      cond do
+        selected_rider = Map.get(assigns.riders, assigns.selected_rider_id) -> selected_rider
+        [{selected_rider, _} | _] = conversations -> selected_rider
+        true -> nil
+    end
+
+    {:ok, socket
+    |> assign(:selected_rider, rider)
+    |> assign(:live_action, assigns.live_action)
+    |> assign(:conversations, conversations)
+    |> assign(:current_user, assigns.current_user)
+    |> assign(:campaign_id, assigns.campaign.id)
+    }
+  end
+
+end
