24931a0541fbc08f4ff15f0bb7cf267a4ef48509
Max Veytsman <maxim@ontoillogical.com>
2024-02-01 21:31:36 -0500

    Update flake

--- flake.nix
+++ flake.nix
@@ -43,6 +43,10 @@
         '';
 
         elixirHook = ''
+          export MIX_HOME=$PWD/.nix-mix
+          export HEX_HOME=$PWD/.nix-hex
+          export PATH=$MIX_HOME/bin:$PATH
+          export PATH=$HEX_HOME/bin:$PATH
           export ERL_AFLAGS="-kernel shell_history enabled"
           export ERL_LIBS="" # see https://elixirforum.com/t/compilation-warnings-clause-cannot-match-in-mix-and-otp-tutorial/25114/4
         '';
