677e053b959738371a2cb4997a4116502e8dcee7
teesloane <tyler.sloane@testdouble.com>
2024-03-26 08:08:02 -0400

    Simplify setup block

--- test/bike_brigade_web/live/rider_home_live_test.exs
+++ test/bike_brigade_web/live/rider_home_live_test.exs
@@ -4,120 +4,93 @@ defmodule BikeBrigadeWeb.RiderHomeLiveTest do
 
   import Phoenix.LiveViewTest
 
+  defp make_campaign(program_id, opts \\ []) do
+    start_offset = Keyword.get(opts, :start_offset, 0)
+    fixture(:campaign, %{
+      program_id: program_id,
+      delivery_start: LocalizedDateTime.now() |> DateTime.add(start_offset, :day),
+      delivery_end: LocalizedDateTime.now() |> DateTime.add(start_offset, :day) |> DateTime.add(1, :hour),
+    })
+  end
+
+
   describe "Rider Home Screen" do
     setup ctx do
       program = fixture(:program, %{name: "ACME Delivery"})
+      program2 = fixture(:program, %{name: "ABC Foodbank"})
       res = login_as_rider(ctx)
-      Map.merge(res, %{program: program})
-    end
 
-    test "it shows am empty state, copy, cta button, when rider has no itinerary", ctx do
-      {:ok, live, html} = live(ctx.conn, ~p"/home")
-      assert html =~ "You do not have any deliveries today."
-      assert html =~ "/images/cyclist-empty-state.svg"
-      assert html =~ "no-campaigns-signup-btn"
-    end
+      rider_2 = fixture(:rider, %{name: "Hannah Bannana"})
+      campaign = make_campaign(program.id)
+      campaign2 = make_campaign(program2.id)
+      campaign3 = make_campaign(program2.id, start_offset: 1)
+      campaign_past_1 = make_campaign(program.id, start_offset: -7)
+      campaign_past_2 = make_campaign(program.id, start_offset: -7)
 
-    test "it shows a call to action for campaigns that need riders TODAY; goes to correct view", ctx do
-      program = fixture(:program, %{name: "ABC Foodbank"})
-
-      campaign =
-        fixture(:campaign, %{
-          program_id: ctx.program.id,
-          delivery_start: LocalizedDateTime.now(),
-          delivery_end: LocalizedDateTime.now() |> DateTime.add(1, :hour)
-        })
-
-      campaign2 =
-        fixture(:campaign, %{
-          program_id: program.id,
-          delivery_start: LocalizedDateTime.now(),
-          delivery_end: LocalizedDateTime.now() |> DateTime.add(1, :hour)
-        })
-
-      campaign3 =
-        fixture(:campaign, %{
-          program_id: program.id,
-          delivery_start: LocalizedDateTime.now() |> DateTime.add(1, :day),
-          delivery_end: LocalizedDateTime.now() |> DateTime.add(1, :day) |> DateTime.add(1, :hour)
-        })
-
+      # We don't assign the rider to these tasks yet because we want to test both empty and filled states.
       fixture(:task, %{campaign: campaign, rider: nil})
       fixture(:task, %{campaign: campaign, rider: nil})
       fixture(:task, %{campaign: campaign2, rider: nil})
       fixture(:task, %{campaign: campaign3, rider: nil})
 
+      # create tasks for campaigns in the last seven days for the stats feature.
+      for _r <- 1..7, do: fixture(:task, %{campaign: campaign_past_1, rider: res.rider})
+      fixture(:task, %{campaign: campaign_past_2, rider: rider_2})
+
+      Map.merge(res, %{
+        program: program,
+        campaign: campaign,
+        campaign2: campaign2,
+        campaign3: campaign3
+      })
+    end
+
+    test "it shows am empty state, copy, cta button, when rider has no itinerary", ctx do
+      {:ok, _live, html} = live(ctx.conn, ~p"/home")
+      assert html =~ "You do not have any deliveries today."
+      assert html =~ "/images/cyclist-empty-state.svg"
+      assert html =~ "no-campaigns-signup-btn"
+    end
+
+    test "it shows a call to action for campaigns that need riders TODAY; goes to correct view",
+         ctx do
       {:ok, live, html} = live(ctx.conn, ~p"/home")
       assert html =~ "4 Unassigned deliveries"
-      assert html =~ "ACME Delivery and ABC Foodbank are looking for riders for the next 48 hours."
+
+      assert html =~
+               "ACME Delivery and ABC Foodbank are looking for riders for the next 48 hours."
 
       live
       |> element("#urgent-campaigns-signup-btn")
       |> render_click()
 
-      expected_redirect = ~p"/campaigns/signup?campaign_ids[]=#{campaign.id}&campaign_ids[]=#{campaign2.id}&campaign_ids[]=#{campaign3.id}"
+      expected_redirect =
+        ~p"/campaigns/signup?campaign_ids[]=#{ctx.campaign.id}&campaign_ids[]=#{ctx.campaign2.id}&campaign_ids[]=#{ctx.campaign3.id}"
+
       assert_redirected(live, expected_redirect)
 
       {:ok, live, html} = live(ctx.conn, expected_redirect)
 
-      assert live |> has_element?("#campaign-#{campaign.id}")
-      assert live |> has_element?("#campaign-#{campaign2.id}")
+      assert live |> has_element?("#campaign-#{ctx.campaign.id}")
+      assert live |> has_element?("#campaign-#{ctx.campaign2.id}")
 
       # assert that only 2 campaigns - the ones with unfilled tasks are showing up.
       assert Floki.parse_document!(html)
-      |> Floki.find(".campaign-item")
-      |> Enum.count()  == 3
+             |> Floki.find(".campaign-item")
+             |> Enum.count() == 3
     end
 
-    test "it shows the riders itinerary when they have deliveries for today, with a sign up button",
-         ctx do
-      program = fixture(:program, %{name: "ABC Foodbank"})
-
-      campaign =
-        fixture(:campaign, %{
-          program_id: ctx.program.id,
-          delivery_start: LocalizedDateTime.now(),
-          delivery_end: LocalizedDateTime.now() |> DateTime.add(1, :hour)
-        })
-
-      campaign2 =
-        fixture(:campaign, %{
-          program_id: program.id,
-          delivery_start: LocalizedDateTime.now(),
-          delivery_end: LocalizedDateTime.now() |> DateTime.add(1, :hour)
-        })
-
-      fixture(:task, %{campaign: campaign, rider: ctx.rider})
-      fixture(:task, %{campaign: campaign2, rider: ctx.rider})
+    test "it shows rider's itinerary of deliveries for today, with a sign up button", ctx do
+      fixture(:task, %{campaign: ctx.campaign, rider: ctx.rider})
+      fixture(:task, %{campaign: ctx.campaign2, rider: ctx.rider})
 
       {:ok, _live, html} = live(ctx.conn, ~p"/home")
+
       assert html =~ "ACME Delivery"
       assert html =~ "ABC Foodbank"
     end
 
     test "it shows a stats based on the last seven days", ctx do
-      program = fixture(:program, %{name: "ABC Foodbank"})
-      rider_2 = fixture(:rider, %{name: "Hannah Bannana"})
-
-      campaign =
-        fixture(:campaign, %{
-          program_id: ctx.program.id,
-          delivery_start: LocalizedDateTime.now() |> DateTime.add(-7, :day),
-          delivery_end:
-            LocalizedDateTime.now() |> DateTime.add(-7, :day) |> DateTime.add(60, :second)
-        })
-
-      campaign2 =
-        fixture(:campaign, %{
-          program_id: program.id,
-          delivery_start: LocalizedDateTime.now() |> DateTime.add(-7, :day),
-          delivery_end:
-            LocalizedDateTime.now() |> DateTime.add(-7, :day) |> DateTime.add(60, :second)
-        })
-
-      for _r <- 1..7, do: fixture(:task, %{campaign: campaign, rider: ctx.rider})
-      fixture(:task, %{campaign: campaign2, rider: rider_2})
-
       {:ok, _live, html} = live(ctx.conn, ~p"/home")
 
       floki_output =
