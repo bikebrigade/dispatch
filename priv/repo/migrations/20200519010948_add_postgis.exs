54355a1b3b90f72457b96d5d88a2042dcc55fa73
Dan Peterson <danp@danp.net>
2024-06-19 20:37:21 -0300

    wip

--- priv/repo/migrations/20200519010948_add_postgis.exs
+++ priv/repo/migrations/20200519010948_add_postgis.exs
@@ -2,7 +2,9 @@ defmodule BikeBrigade.Repo.Migrations.AddPostgis do
   use Ecto.Migration
 
   def up do
+    execute "set pgaudit.log='none'"
     execute "CREATE EXTENSION postgis"
+    execute "set pgaudit.log='ddl'"
   end
 
   def down do
