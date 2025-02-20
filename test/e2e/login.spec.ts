3b40fff94c059e846eb28a8c17cbcc7f04b7c8ed
Ty <tylersloane@gmail.com>
2025-02-19 19:53:51 -0500

    Add: test for new campaigns.

--- test/e2e/login.spec.ts
+++ test/e2e/login.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect, Page } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
-const programName = `Test Program ${Date.now()}`;
+const programName = faker.company.name()
 
 test.describe('Login and Logout', () => {
   test('Can Login', async ({ page }) => {
@@ -62,46 +63,51 @@ test.describe('Programs', () => {
 
 test.describe('Campaigns', () => {
   let page: Page
+  let programName: string
 
   test.beforeAll(async ({ browser }) => {
-    const programName = `Test Program for New Campaign ${Date.now()}`;
+    programName = faker.company.name()
     page = await browser.newPage()
     await doLogin(page)
     await createProgram(page, programName)
   })
 
+
   test('Can create a campaign', async () => {
     await page.goto('http://localhost:4000/campaigns/new');
+    await page.waitForSelector("body > .phx-connected")
     await page.getByRole('textbox', { name: 'Delivery Date' }).fill(getDatePlusDays(0));
-    // TODO: this won't select option by label, meaning it's selecting an arbitrary program.
-    await page.locator('#user-form_program_id').selectOption("3"); // << this works but isn't ideal
+    await page.locator('#user-form_program_id').click()
+    // HACK: a way to target the specific program, since we can't select by value for some reason.
+    await page.locator('#user-form_program_id').pressSequentially(programName, { delay: 30 })
+    await page.locator('#user-form_program_id').press('Enter')
+
+
     await page.locator('#location-form-location-input-open').click();
-    await page.locator('[id="campaign_form\\[location\\]_address"]').click();
-    await page.locator('[id="campaign_form\\[location\\]_address"]').fill('123 yonge');
-    await page.waitForTimeout(2000);
+    await page.locator('#location-form-location-input-open').pressSequentially("200 Yonge", { delay: 100 })
     await page.getByRole('button', { name: 'Save' }).click();
-    await expect(page.getByText('Success! Campaign created')).toBeVisible({ timeout: 10000 });
+    await expect(page.locator('#flash')).toContainText('Success! Campaign created successfully');
+    await expect(page.getByText(programName)).toBeVisible();
   })
 
   test('Can create a campaign for next week', async () => {
     await page.goto('http://localhost:4000/campaigns/new');
+    await page.waitForSelector("body > .phx-connected")
     await page.getByRole('textbox', { name: 'Delivery Date' }).fill(getDatePlusDays(8));
-    // TODO: this won't select option by label, meaning it's selecting an arbitrary program.
-    await page.locator('#user-form_program_id').selectOption({label: programName});
-    // await page.locator('#user-form_program_id').selectOption("3");
+    await page.locator('#user-form_program_id').click()
+    await page.locator('#user-form_program_id').pressSequentially(programName, { delay: 30 })
+    await page.locator('#user-form_program_id').press('Enter')
     await page.locator('#location-form-location-input-open').click();
-    await page.locator('[id="campaign_form\\[location\\]_address"]').click();
-    await page.locator('[id="campaign_form\\[location\\]_address"]').fill('123 yonge');
-    await page.waitForTimeout(2000);
+    await page.locator('#location-form-location-input-open').pressSequentially("200 Yonge", { delay: 100 })
     await page.getByRole('button', { name: 'Save' }).click();
-    await expect(page.getByText('Success! Campaign created')).toBeVisible({ timeout: 10000 });
+    await expect(page.locator('#flash')).toContainText('Success! Campaign created successfully');
+
     // now go check that campaign shows up next week.
     await page.getByRole('link', { name: 'Campaigns' }).click();
     // goto next week
     await page.getByRole('navigation', { name: 'Pagination' }).getByRole('link').nth(2).click();
     // ensure that new campaign for next-week is present
-    await expect(page.locator('#campaign-4')).toContainText('Test Program 1739811044662 edited');
-    await expect(page.getByRole('link', { name: 'Test Program 1739811044662' })).toBeVisible();
+    await expect(page.getByText(programName)).toBeVisible();
   })
 })
 
@@ -114,22 +120,6 @@ async function doLogin(page: any) {
   await page.getByRole('textbox', { name: 'Authentication Code' }).fill('123456');
   await page.getByRole('button', { name: 'Sign in' }).click();
 }
-
-
-// async function createCampaign(page: any) {
-//   await page.getByRole('link', { name: 'Campaigns' }).click();
-//   await page.getByRole('link', { name: 'New Campaign' }).click();
-//   await page.getByRole('textbox', { name: 'Delivery Date' }).fill(getDatePlusDays(0));
-//   await page.locator('#user-form_program_id').selectOption({});
-//   await page.locator('#location-form-location-input-open').click();
-//   await page.locator('[id="campaign_form\\[location\\]_address"]').click();
-//   await page.locator('[id="campaign_form\\[location\\]_address"]').fill('123 yonge');
-//   await page.waitForTimeout(2000);
-//   await page.getByRole('button', { name: 'Save' }).click();
-//   await expect(page.getByText('Success! Campaign created')).toBeVisible({ timeout: 10000 });
-// }
-
-// async function createCampaignForNextWeek(page: any) { }
 
 
 function getDatePlusDays(daysToAdd: number) {
