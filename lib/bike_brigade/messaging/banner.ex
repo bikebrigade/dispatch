709845b098312aff5edf90c5e14fba33d171cd64
Ty <tylersloane@gmail.com>
2025-04-07 16:29:08 -0400

    Add: banner schema and changeset.

---
+++ lib/bike_brigade/messaging/banner.ex
@@ -0,0 +1,26 @@
+defmodule BikeBrigade.Messaging.Banner do
+  use BikeBrigade.Schema
+  import Ecto.Changeset
+
+  alias BikeBrigade.Accounts.User
+
+  schema "banners" do
+    field :message, :string
+    field :turn_on_at, :utc_datetime
+    field :turn_off_at, :utc_datetime
+    belongs_to :created_by, User
+  end
+
+  @doc false
+  def changeset(banner, attrs) do
+    banner
+    |> cast(attrs, [
+      :message,
+      :created_by_id,
+      :turn_on_at,
+      :turn_off_at,
+    ])
+    |> validate_required([:message, :created_by_id, :turn_on_at, :turn_off_at])
+  end
+  
+end
