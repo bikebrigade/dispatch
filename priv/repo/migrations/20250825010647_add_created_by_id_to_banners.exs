e86d5f2b05206a65847852c1989dcdd48507bc05
Ty <tylersloane@gmail.com>
2025-08-24 21:18:15 -0400

    Remove index, who needs it.

--- priv/repo/migrations/20250825010647_add_created_by_id_to_banners.exs
+++ priv/repo/migrations/20250825010647_add_created_by_id_to_banners.exs
@@ -5,7 +5,5 @@ defmodule BikeBrigade.Repo.Migrations.AddCreatedByIdToBanners do
     alter table(:banners) do
       add :created_by_id, references(:users), null: true
     end
-
-    create index(:banners, [:created_by_id])
   end
 end
