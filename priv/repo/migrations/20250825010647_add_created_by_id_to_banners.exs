4256364d14d0d66e988da2cb0b50c80aeea7389c
Ty <tylersloane@gmail.com>
2025-08-24 21:56:20 -0400

    Use correct function

--- priv/repo/migrations/20250825010647_add_created_by_id_to_banners.exs
+++ priv/repo/migrations/20250825010647_add_created_by_id_to_banners.exs
@@ -2,11 +2,8 @@ defmodule BikeBrigade.Repo.Migrations.AddCreatedByIdToBanners do
   use Ecto.Migration
 
   def change do
-    # Only add the column if it doesn't exist
-    unless column_exists?(:banners, :created_by_id) do
-      alter table(:banners) do
-        add :created_by_id, references(:users), null: true
-      end
+    alter table(:banners) do
+      add_if_not_exists :created_by_id, references(:users), null: true
     end
   end
 end
