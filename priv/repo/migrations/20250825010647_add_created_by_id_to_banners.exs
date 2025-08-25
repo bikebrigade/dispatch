2bff6b9cbe4959082566bf1131d3f47dac70794d
Ty <tylersloane@gmail.com>
2025-08-24 21:33:45 -0400

    Make migration conditional so it passes CI.

--- priv/repo/migrations/20250825010647_add_created_by_id_to_banners.exs
+++ priv/repo/migrations/20250825010647_add_created_by_id_to_banners.exs
@@ -2,8 +2,11 @@ defmodule BikeBrigade.Repo.Migrations.AddCreatedByIdToBanners do
   use Ecto.Migration
 
   def change do
-    alter table(:banners) do
-      add :created_by_id, references(:users), null: true
+    # Only add the column if it doesn't exist
+    unless column_exists?(:banners, :created_by_id) do
+      alter table(:banners) do
+        add :created_by_id, references(:users), null: true
+      end
     end
   end
 end
