3f5aca628b34ab996e06a1dc9f179119193e330d
Neel Vinoth <neel_bike@MacBook-Air.local>
2026-01-22 20:43:12 -0500

    Refactored to improve code readability

--- lib/bike_brigade/prom_ex.ex
+++ lib/bike_brigade/prom_ex.ex
@@ -1,34 +1,34 @@
 defmodule BikeBrigade.PromEx do
-  use PromEx, otp_app: :todo_list
+  # use PromEx, otp_app: :todo_list
 
-  alias PromEx.Plugins
+  # alias PromEx.Plugins
 
-  @impl true
-  def plugins do
-    [
-      # PromEx built in plugins
-      Plugins.Application,
-      Plugins.Beam,
-      {Plugins.Phoenix, router: TodoListWeb.Router},
-      Plugins.PhoenixLiveView
-    ]
-  end
+  # @impl true
+  # def plugins do
+  #   [
+  #     # PromEx built in plugins
+  #     Plugins.Application,
+  #     Plugins.Beam,
+  #     {Plugins.Phoenix, router: TodoListWeb.Router},
+  #     Plugins.PhoenixLiveView
+  #   ]
+  # end
 
-  @impl true
-  def dashboard_assigns do
-    [
-      datasource_id: "prometheus"
-    ]
-  end
+  # @impl true
+  # def dashboard_assigns do
+  #   [
+  #     datasource_id: "prometheus"
+  #   ]
+  # end
 
-  @impl true
-  def dashboards do
-    [
-      # PromEx built in Grafana dashboards
-      {:prom_ex, "application.json"},
-      {:prom_ex, "beam.json"},
-      {:prom_ex, "phoenix.json"},
-      {:prom_ex, "phoenix_live_view.json"}
-    ]
-  end
+  # @impl true
+  # def dashboards do
+  #   [
+  #     # PromEx built in Grafana dashboards
+  #     {:prom_ex, "application.json"},
+  #     {:prom_ex, "beam.json"},
+  #     {:prom_ex, "phoenix.json"},
+  #     {:prom_ex, "phoenix_live_view.json"}
+  #   ]
+  # end
 end
