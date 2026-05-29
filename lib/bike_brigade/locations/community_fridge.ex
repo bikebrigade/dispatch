edee21d513f0bccfe27223dc5e49edf54aa1c48c
Neel Vinoth <nealbpm6@gmail.com>
2026-05-29 15:03:58 -0400

    CommunityFridge and Task relationship made explicit

--- lib/bike_brigade/locations/community_fridge.ex
+++ lib/bike_brigade/locations/community_fridge.ex
@@ -3,6 +3,7 @@ defmodule BikeBrigade.Locations.CommunityFridge do
   import Ecto.Changeset
 
   alias BikeBrigade.Locations.Location
+  alias BikeBrigade.Delivery.Task
 
   schema "community_fridges" do
     field :active, :boolean, default: true
@@ -11,6 +12,8 @@ defmodule BikeBrigade.Locations.CommunityFridge do
     field :photo, :string
     field :pair_preferred, :boolean, default: false
     belongs_to :location, Location
+
+    has_many :tasks, Task
 
     timestamps()
   end
