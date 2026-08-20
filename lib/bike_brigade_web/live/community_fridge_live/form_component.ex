2a368fc77ae7b9fa3d8ca8114bcaf8b3d6a4d4f8
Neel Vinoth <nealbpm6@gmail.com>
2026-08-20 16:50:26 -0400

    Add photo upload support to CommunityFridgeLive.FormComponent
      Single photo upload via MediaStorage, matching   the program form pattern.
      Shows current photo with inline × removal, drag-and-drop upload zone with live preview, and explicitly persists photo deletion to the DB on save.

--- lib/bike_brigade_web/live/community_fridge_live/form_component.ex
+++ lib/bike_brigade_web/live/community_fridge_live/form_component.ex
@@ -1,7 +1,12 @@
 defmodule BikeBrigadeWeb.CommunityFridgeLive.FormComponent do
   use BikeBrigadeWeb, :live_component
 
-  alias BikeBrigade.Locations
+  alias BikeBrigade.{Locations, MediaStorage}
+
+  @impl true
+  def mount(socket) do
+    {:ok, allow_upload(socket, :photo, accept: ~w(.gif .png .jpg .jpeg), max_entries: 1)}
+  end
 
   @impl true
   def update(%{community_fridge: community_fridge} = assigns, socket) do
@@ -23,7 +28,37 @@ defmodule BikeBrigadeWeb.CommunityFridgeLive.FormComponent do
     {:noreply, assign(socket, :changeset, changeset)}
   end
 
+  def handle_event("remove_photo", _params, socket) do
+    changeset = Ecto.Changeset.put_change(socket.assigns.changeset, :photo, nil)
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
+
+  def handle_event("cancel_upload", %{"ref" => ref}, socket) do
+    {:noreply, cancel_upload(socket, :photo, ref)}
+  end
+
   def handle_event("save", %{"community_fridge" => community_fridge_params}, socket) do
+    photo_url =
+      case consume_uploaded_entries(socket, :photo, fn %{path: path},
+                                                       %{client_type: content_type} ->
+             {:ok, MediaStorage.upload_file!(path, content_type)}
+           end) do
+        [%{url: url}] -> url
+        [] -> nil
+      end
+
+    community_fridge_params =
+      cond do
+        photo_url ->
+          Map.put(community_fridge_params, "photo", photo_url)
+
+        match?(%{changes: %{photo: nil}}, socket.assigns.changeset) ->
+          Map.put(community_fridge_params, "photo", nil)
+
+        true ->
+          community_fridge_params
+      end
+
     save_community_fridge(socket, socket.assigns.action, community_fridge_params)
   end
 
@@ -55,4 +90,8 @@ defmodule BikeBrigadeWeb.CommunityFridgeLive.FormComponent do
         {:noreply, assign(socket, changeset: changeset)}
     end
   end
+
+  defp error_to_string(:too_large), do: "Too large"
+  defp error_to_string(:too_many_files), do: "You have selected too many files"
+  defp error_to_string(:not_accepted), do: "You have selected an unacceptable file type"
 end
