f01ac3eee0ad13be659dc320b23f7aa635e36521
Neel Vinoth <nealbpm6@gmail.com>
2026-09-22 13:31:15 -0400

    Add Remove button to rider edit slideover
      Wires the existing Riders.remove_rider/1 soft-delete function into the dispatcher UI so riders can be removed without involving the technical team. Navigates back to /riders after removal since the profile page no  longer represents a valid resource.

--- lib/bike_brigade_web/live/rider_live/show.ex
+++ lib/bike_brigade_web/live/rider_live/show.ex
@@ -24,6 +24,16 @@ defmodule BikeBrigadeWeb.RiderLive.Show do
   end
 
   @impl Phoenix.LiveView
+  def handle_event("delete", %{"id" => id}, socket) do
+    rider = Riders.get_rider!(id)
+    {:ok, _} = Riders.remove_rider(rider)
+
+    {:noreply,
+     socket
+     |> put_flash(:info, "Rider removed")
+     |> push_navigate(to: ~p"/riders")}
+  end
+
   def handle_event("prev_schedule", %{"period" => period}, socket) do
     [{date, _}, _, _] = socket.assigns.schedule
 
